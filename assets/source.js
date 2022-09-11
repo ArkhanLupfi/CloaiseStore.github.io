5a6d7dbfd0d5508c602288f815abbcb17db30e56
ArkhanLupfi <arkhanlupfi6@gmail.com>
2022-09-11 20:12:09 +0700

    mengubah no wa

--- assets/source.js
+++ assets/source.js
@@ -2,12 +2,12 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 function chat(product_name) {
     const message = `Hello, I want to ask if this ${product_name} is still available?`
-    const whatsapp = `https://wa.me/6281804831455?text=${message}`
+    const whatsapp = `https://wa.me/6285280136173?text=${message}`
     open(whatsapp, "blank")
 }
 
 function buy(product_name, product_price) {
     const message = `Hello I want to buy this ${product_name} - ${product_price}, please confirm my order!`
-    const whatsapp = `https://wa.me/6281804831455?text=${message}`
+    const whatsapp = `https://wa.me/6285280136173?text=${message}`
     open(whatsapp, "blank")
 }
